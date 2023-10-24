c2a51e0a74c93a326a9ab367627ecf40fa372dd2
4ndrelo <allanda23456@gmail.com>
2023-10-24 07:49:19 -0600

    Ejercicio 1 y 2

--- PAT_Parcial_01_A/Ejercicio01.cpp
+++ PAT_Parcial_01_A/Ejercicio01.cpp
@@ -2,5 +2,27 @@
 
 Node<char>* Ejercicio01::remove(Node<char>* head, char value)
 {
-    return nullptr;
+    if (head == nullptr) {
+        return nullptr;
+    }
+
+    while (head != nullptr && head->value == value) {
+        Node<char>* temp = head;
+        head = head->next;
+        delete temp;
+    }
+
+    Node<char>* current = head;
+    while (current != nullptr && current->next != nullptr) {
+        if (current->next->value == value) {
+            Node<char>* temp = current->next;
+            current->next = temp->next;
+            delete temp;
+        }
+        else {
+            current = current->next;
+        }
+    }
+
+    return head;
 }
--- PAT_Parcial_01_A/Ejercicio02.h
+++ PAT_Parcial_01_A/Ejercicio02.h
@@ -1,12 +1,24 @@
 #pragma once
 
 #include <vector>
-
-using std::vector;
+#include <iostream>
+using namespace std;
 
 class Ejercicio02
 {
 public:
-	int countStudents(vector<char>* students, vector<char>* sandwiches);
-};
+    int countStudents(vector<char> students, vector<char> sandwiches)
+    {
+        int studentIndex = 0;
+        int sandwichIndex = 0;
 
+        while (studentIndex < students.size() && sandwichIndex < sandwiches.size()) {
+            if (students[studentIndex] == sandwiches[sandwichIndex]) {
+                
+                studentIndex++;
+            }
+            sandwichIndex++;
+        }
+        return students.size() - studentIndex;
+    }
+};
