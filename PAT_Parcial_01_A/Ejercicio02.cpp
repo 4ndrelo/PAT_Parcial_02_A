012c033dfeebf7836c1ecf282fddb6fd60bd9c01
4ndrelo <allanda23456@gmail.com>
2023-10-24 08:03:36 -0600

    Ejercicio 1 y 2

--- PAT_Parcial_01_A/Ejercicio02.cpp
+++ PAT_Parcial_01_A/Ejercicio02.cpp
@@ -2,18 +2,23 @@
 
 int Ejercicio02::countStudents(vector<char>* students, vector<char>* sandwiches)
 {
-
     int studentIndex = 0;
     int sandwichIndex = 0;
 
     while (studentIndex < students.size() && sandwichIndex < sandwiches.size()) {
         if (students[studentIndex] == sandwiches[sandwichIndex]) {
-         
+    
             studentIndex++;
         }
         sandwichIndex++;
     }
 
-   
-    return students.size() - studentIndex;
+    
+    vector<char> studentsDidNotEat;
+    while (studentIndex < students.size()) {
+        studentsDidNotEat.push_back(students[studentIndex]);
+        studentIndex++;
+    }
+
+    return studentsDidNotEat;
 }
